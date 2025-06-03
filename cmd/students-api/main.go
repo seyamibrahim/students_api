44ed0e48c467cb294784a6fe3b6480c4416c9fd6
seyam <seyamibrahim88@gmail.com>
2025-06-03 16:25:04 +0530

    connect with database

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -13,18 +13,31 @@ import (
 
 	"github.com/seyamibrahim/students-api/internal/config"
 	"github.com/seyamibrahim/students-api/internal/http/handlers/student"
+	"github.com/seyamibrahim/students-api/internal/storage/sqlite"
 )
 
 func main() {
 	// load config
 
 	cfg := config.MustLoad()
+
+
+
 	// database setup
+
+	storage, err := sqlite.New(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+
 
 	// setup router
 
 	router := http.NewServeMux()
-	router.HandleFunc("POST /api/students",student.New())
+	router.HandleFunc("POST /api/students",student.New(storage))
+	router.HandleFunc("GET /api/students/{id}",student.GetById(storage))
 
 	//setup server
 
--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -9,11 +9,12 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/seyamibrahim/students-api/internal/storage"
 	"github.com/seyamibrahim/students-api/internal/types"
 	"github.com/seyamibrahim/students-api/utils/response"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		slog.Info("creating a student")
@@ -39,6 +40,19 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "Ok"})
+		lastId, err := storage.CreateStudent(student.Name,
+			student.Email, student.Age)
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		
+		slog.Info("user created successfully", slog.String("userId", fmt.Sprint(lastId)))
+
+
+
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 	}
 }
